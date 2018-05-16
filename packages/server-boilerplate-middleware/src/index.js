004e7ac41a8443cc48a9f696f1d376654eb844ab
zephinzer <dev@joeir.net>
2018-05-16 10:58:02 +0800

    added cookie parsing abilities (and linter)

--- packages/server-boilerplate-middleware/src/index.js
+++ packages/server-boilerplate-middleware/src/index.js
@@ -3,6 +3,13 @@ import cookieParser from 'cookie-parser';
 
 module.exports = createServer;
 
+/**
+ * Returns a server that is:
+ *  - compatible with Express.js interfaces
+ *  - able to parse cookies
+ *
+ * @return {express.Application}
+ */
 export default function createServer() {
   const server = express();
   server.use(cookieParser());
--- packages/server-boilerplate-middleware/src/index.test.js
+++ packages/server-boilerplate-middleware/src/index.test.js
@@ -8,40 +8,58 @@ describe('createServer()', () => {
 
   it('returns an Express compatible server', () => {
     const boilerplateServer = createServer();
-    boilerplateServer.get('/get', (req, res) => res.json(`${TEST_STRING} /get`));
-    boilerplateServer.post('/post', (req, res) => res.json(`${TEST_STRING} /post`));
-    boilerplateServer.delete('/delete', (req, res) => res.json(`${TEST_STRING} /delete`));
-    boilerplateServer.put('/put', (req, res) => res.json(`${TEST_STRING} /put`));
-    boilerplateServer.patch('/patch', (req, res) => res.json(`${TEST_STRING} /patch`));
-    boilerplateServer.options('/options', (req, res) => res.json(`${TEST_STRING} /options`));
-    boilerplateServer.head('/head', (req, res) => res.status(TEST_HTTP_CODE).end());
-    boilerplateServer.use((req, res) => res.json(TEST_STRING));
     expect(boilerplateServer.listen).to.be.a('function');
+
+    boilerplateServer
+      .get('/get', (req, res) => res.json(`${TEST_STRING} /get`));
+    boilerplateServer
+      .post('/post', (req, res) => res.json(`${TEST_STRING} /post`));
+    boilerplateServer
+      .delete('/delete', (req, res) => res.json(`${TEST_STRING} /delete`));
+    boilerplateServer
+      .put('/put', (req, res) => res.json(`${TEST_STRING} /put`));
+    boilerplateServer
+      .patch('/patch', (req, res) => res.json(`${TEST_STRING} /patch`));
+    boilerplateServer
+      .options('/options', (req, res) => res.json(`${TEST_STRING} /options`));
+    boilerplateServer
+      .head('/head', (req, res) => res.status(TEST_HTTP_CODE).end());
+    boilerplateServer
+      .use((req, res) => res.json(TEST_STRING));
+
     return Promise.all([
-      supertest(boilerplateServer).get('/').expect(200).then((res) => {
-        expect(res.text).to.eql(JSON.stringify(TEST_STRING));
-      }),
-      supertest(boilerplateServer).get('/get').expect(200).then((res) => {
-        expect(res.text).to.eql(JSON.stringify(`${TEST_STRING} /get`));
-      }),
-      supertest(boilerplateServer).post('/post').expect(200).then((res) => {
-        expect(res.text).to.eql(JSON.stringify(`${TEST_STRING} /post`));
-      }),
-      supertest(boilerplateServer).put('/put').expect(200).then((res) => {
-        expect(res.text).to.eql(JSON.stringify(`${TEST_STRING} /put`));
-      }),
-      supertest(boilerplateServer).delete('/delete').expect(200).then((res) => {
-        expect(res.text).to.eql(JSON.stringify(`${TEST_STRING} /delete`));
-      }),
-      supertest(boilerplateServer).patch('/patch').expect(200).then((res) => {
-        expect(res.text).to.eql(JSON.stringify(`${TEST_STRING} /patch`));
-      }),
-      supertest(boilerplateServer).options('/options').expect(200).then((res) => {
-        expect(res.text).to.eql(JSON.stringify(`${TEST_STRING} /options`));
-      }),
-      supertest(boilerplateServer).head('/head').expect(418),
-    ]);
-  });
+      supertest(boilerplateServer)
+        .get('/').expect(200).then((res) => {
+          expect(res.text).to.eql(JSON.stringify(TEST_STRING));
+        }),
+      supertest(boilerplateServer)
+        .get('/get').expect(200).then((res) => {
+          expect(res.text).to.eql(JSON.stringify(`${TEST_STRING} /get`));
+        }),
+      supertest(boilerplateServer)
+        .post('/post').expect(200).then((res) => {
+          expect(res.text).to.eql(JSON.stringify(`${TEST_STRING} /post`));
+        }),
+      supertest(boilerplateServer)
+        .put('/put').expect(200).then((res) => {
+          expect(res.text).to.eql(JSON.stringify(`${TEST_STRING} /put`));
+        }),
+      supertest(boilerplateServer)
+        .delete('/delete').expect(200).then((res) => {
+          expect(res.text).to.eql(JSON.stringify(`${TEST_STRING} /delete`));
+        }),
+      supertest(boilerplateServer)
+        .patch('/patch').expect(200).then((res) => {
+          expect(res.text).to.eql(JSON.stringify(`${TEST_STRING} /patch`));
+        }),
+      supertest(boilerplateServer)
+        .options('/options').expect(200).then((res) => {
+          expect(res.text).to.eql(JSON.stringify(`${TEST_STRING} /options`));
+        }),
+      supertest(boilerplateServer)
+        .head('/head').expect(418),
+      ]);
+    });
 
   it('can be imported using require', () => {
     expect(createServer).to.eql(require('./index'));
