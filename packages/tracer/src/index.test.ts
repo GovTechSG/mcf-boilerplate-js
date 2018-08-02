19587bdffb983c4429751bb646270822e85a22eb
zephinzer <gitlab@joeir.net>
2018-08-02 15:14:46 +0800

    added @mcf/tracer package - created zipkin provisioining compose file - disabled `noImplicitAny`

---
+++ packages/tracer/src/index.test.ts
@@ -0,0 +1,112 @@
+import * as chai from 'chai';
+import express from 'express';
+import * as superagent from 'superagent';
+import supertest from 'supertest';
+import {ExplicitContext} from 'zipkin';
+import {
+  createTracer,
+  IContextShape,
+  IExpressRequestWithContext,
+  ITracer,
+} from './';
+
+const {expect} = chai;
+
+describe('@mcf/tracer', () => {
+  let waitingForZipkin;
+
+  before(function() {
+    this.timeout(30000);
+    process.stdout.write('(waiting for zipkin...');
+    return new Promise((resolve, reject) => {
+      (function waitForZipkin() {
+        superagent
+          .get('http://localhost:9411')
+          .then((response) => {
+            if (response.status === 200) {
+              process.stdout.write(' done)\n');
+              clearTimeout(waitingForZipkin);
+              resolve();
+            } else {
+              process.stdout.write('.');
+              waitingForZipkin = setTimeout(waitForZipkin, 1000);
+            }
+          })
+          .catch((error) => {
+            process.stdout.write('.');
+            waitingForZipkin = setTimeout(waitForZipkin, 1000);
+          });
+      })();
+    });
+  });
+
+  describe('integration tests', () => {
+    let server: express.Application;
+    let tracer: ITracer;
+    let observedTraceFromRequest: IContextShape | undefined;
+    let observedTraceFromContext: IContextShape;
+
+    before(() => {
+      server = express();
+      tracer = createTracer({
+        sampleRate: 1,
+        serverHost: 'localhost',
+        serverPort: '9411',
+      });
+    });
+
+    describe('.getContext()', () => {
+      it('retrieves the context in use by the tracer', () => {
+        expect(() => {
+          tracer.getContext();
+        }).to.not.throw();
+      });
+    });
+
+    describe('.getExpressMiddleware()', () => {
+      before(() => {
+        server.use(tracer.getExpressMiddleware());
+        server.get(
+          '/_get_express_middleware',
+          (req: IExpressRequestWithContext, res: express.Response) => {
+            res.json({...req.context});
+          },
+        );
+      });
+
+      it('adds a .context property to the request object', () =>
+        supertest(server)
+          .get('/_get_express_middleware')
+          .expect(200)
+          .then(({body}) => {
+            expect(body).to.exist;
+          }));
+
+      it('sends the correct information to zipkin', () =>
+        supertest(server)
+          .get('/_get_express_middleware')
+          .expect(200)
+          .then(({body}) => body.traceId)
+          .then(
+            (traceId) =>
+              new Promise((resolve, reject) => {
+                setTimeout(resolve.bind(null, traceId), 1000);
+              }),
+          )
+          .then((traceId) =>
+            superagent.get(`http://localhost:9411/api/v2/trace/${traceId}`),
+          )
+          .catch((err) => {
+            throw new Error("Zipkin didn't receive trace ID");
+          }));
+    });
+
+    describe('.getTracer()', () => {
+      it('retrieves the tracer instance', () => {
+        expect(() => {
+          tracer.getTracer();
+        }).to.not.throw();
+      });
+    });
+  });
+});
---
+++ packages/tracer/src/index.ts
@@ -0,0 +1,158 @@
+import express from 'express';
+import os from 'os';
+import {
+  BatchRecorder,
+  ConsoleRecorder,
+  ExplicitContext,
+  jsonEncoder,
+  option,
+  Recorder,
+  sampler,
+  Tracer,
+} from 'zipkin';
+import {expressMiddleware} from 'zipkin-instrumentation-express';
+import {HttpLogger as ZipkinHttpTransport} from 'zipkin-transport-http';
+
+const DEFAULT_HTTP_HEADERS = {};
+const DEFAULT_LOCAL_SERVICE_NAME =
+  os.hostname() || process.env.HOSTNAME || 'unknown';
+const DEFAULT_SAMPLE_RATE = 0.5;
+const DEFAULT_SYNC_INTERVAL_MS = 1000;
+const DEFAULT_SERVER_HOST = 'localhost';
+const DEFAULT_SERVER_PORT = '9411';
+const DEFAULT_SERVER_PROTOCOL = 'http';
+
+const {JSON_V2} = jsonEncoder;
+const {CountingSampler} = sampler;
+
+/**
+ * Returns a tracer object
+ *
+ * @param {Object} options
+ * @param {String} options.httpHeaders
+ * @param {String} options.localServiceName
+ * @param {Number} options.sampleRate
+ * @param {Number} options.syncIntervalMs
+ * @param {String} options.serverHost
+ * @param {Number} options.serverPort
+ * @param {String} options.serverProtocol
+ * @return {Object}
+ */
+export function createTracer({
+  httpHeaders = DEFAULT_HTTP_HEADERS,
+  localServiceName = DEFAULT_LOCAL_SERVICE_NAME,
+  sampleRate = DEFAULT_SAMPLE_RATE,
+  syncIntervalMs = DEFAULT_SYNC_INTERVAL_MS,
+  serverHost = DEFAULT_SERVER_HOST,
+  serverPort = DEFAULT_SERVER_PORT,
+  serverProtocol = DEFAULT_SERVER_PROTOCOL,
+}: ICreateTracerParameters = {}): ITracer {
+  const context = new ExplicitContext();
+  const recorder = createRecorder({
+    httpHeaders,
+    serverHost,
+    serverPort,
+    serverProtocol,
+    syncIntervalMs,
+  });
+  const samplerInstance = new CountingSampler(sampleRate);
+  const tracer = new Tracer({
+    ctxImpl: context,
+    localServiceName,
+    recorder,
+    sampler: samplerInstance,
+    traceId128Bit: true,
+  });
+  const middleware = [
+    expressMiddleware({tracer}),
+    contextProviderMiddleware({context}),
+  ].filter((v) => v);
+
+  return {
+    getContext: () => context,
+    getExpressMiddleware: () => middleware,
+    getTracer: () => tracer,
+  };
+}
+
+const contextProviderMiddleware = ({
+  context,
+}: IContextProviderMiddlewareParameters): IExpressHandlerWithContext => (
+  req: IExpressRequestWithContext,
+  res: express.Response,
+  next: express.NextFunction,
+) => {
+  const {spanId, parentId, traceId, sampled} = context.getContext();
+  req.context = {spanId, parentId, traceId, sampled};
+  next();
+};
+
+function createRecorder({
+  httpHeaders = DEFAULT_HTTP_HEADERS,
+  serverHost = DEFAULT_SERVER_HOST,
+  serverPort = DEFAULT_SERVER_PORT,
+  serverProtocol = DEFAULT_SERVER_PROTOCOL,
+  syncIntervalMs = DEFAULT_SYNC_INTERVAL_MS,
+}: ICreateLoggerParameters = {}): Recorder {
+  if (serverHost) {
+    const zipkinHostname = `${serverHost}:${serverPort}`;
+    const zipkinBaseUrl = `${serverProtocol}://${zipkinHostname}`;
+    return new BatchRecorder({
+      logger: new ZipkinHttpTransport({
+        endpoint: `${zipkinBaseUrl}/api/v2/spans`,
+        headers: httpHeaders,
+        httpInterval: syncIntervalMs,
+        jsonEncoder: JSON_V2,
+      }),
+    });
+  } else {
+    return new ConsoleRecorder();
+  }
+}
+
+export interface IContextProviderMiddlewareParameters {
+  context: ExplicitContext;
+}
+
+export interface IContextShape {
+  parentId: string;
+  sampled: option.IOption<boolean>;
+  spanId: string;
+  traceId: string;
+}
+
+export interface IExpressRequestWithContext extends express.Request {
+  context?: IContextShape;
+}
+
+export interface IExpressHandlerWithContext extends express.RequestHandler {
+  (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ): void;
+}
+
+export interface ICreateLoggerParameters {
+  httpHeaders?: object;
+  serverHost?: string;
+  serverPort?: string;
+  serverProtocol?: string;
+  syncIntervalMs?: number;
+}
+
+export interface ICreateTracerParameters {
+  httpHeaders?: object;
+  localServiceName?: string;
+  sampleRate?: number;
+  syncIntervalMs?: number;
+  serverHost?: string;
+  serverPort?: string;
+  serverProtocol?: string;
+}
+
+export interface ITracer {
+  getContext: () => ExplicitContext;
+  getExpressMiddleware: () => IExpressHandlerWithContext[];
+  getTracer: () => Tracer;
+}
