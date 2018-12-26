ac232078afd10b4013294c939b117ea7163c9a60
nebulis <nebounet@gmail.com>
2018-12-26 11:38:18 +0800

    chore: add namespace + silent options and change default format for logger

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,4 +1,4 @@
-import {TransformableInfo, TransformFunction} from 'logform';
+import {TransformableInfo} from 'logform';
 import winston from 'winston';
 import * as Transport from 'winston-transport';
 import {createMorganStream} from './streams';
@@ -11,7 +11,7 @@ export {
   createMorganStream,
 };
 
-const {combine, timestamp, json} = winston.format;
+const {combine, timestamp, json, colorize, printf, label} = winston.format;
 
 // tslint:disable object-literal-sort-keys
 // using value order rather than key order makes more sense
@@ -42,29 +42,49 @@ export interface ILoggerOptions {
   formatters?: IExtendedTransformFunction[];
   level?: LogLevelType;
   transports?: Transport[];
-  additionalTransports?: Transport[];
+  additionalTransports?: Transport | Transport[];
+  namespace?: string;
+  silent?: boolean;
 }
 export interface IApplicationLogger extends winston.Logger {
   getStream: (level: LogLevelType) => {write: any};
 }
+
+const magenta = (str: string) => `\u001b[35m${str}\u001b[39m`;
+const identity = (i) => i;
+const mcfDevelopmentFormat = printf(
+  (info) =>
+    `[${magenta(info.label)}] ${info.timestamp} ${info.level}: ${
+      typeof info.message === 'string'
+        ? info.message
+        : JSON.stringify(info.message)
+    }`,
+);
 
 function createLogger({
   formatters = [],
   level = defaultLevel,
   transports = defaultTransports,
   additionalTransports = defaultAdditionalTransports,
+  namespace = '',
+  silent = false,
 }: ILoggerOptions = {}): IApplicationLogger {
   const logger = winston.createLogger({
     exitOnError: false,
     format: combine(
+      process.env.NODE_ENV === 'production' // add colors for development
+        ? winston.format(identity)()
+        : colorize(),
       ...formatters
         .map((formatter) => winston.format(formatter))
         .map((fn) => fn()), // need the second call to unwrap the formatter
+      label({label: namespace}),
       timestamp(),
-      json(),
+      process.env.NODE_ENV === 'production' ? json() : mcfDevelopmentFormat,
     ),
     level,
     levels: defaultLevels,
+    silent,
     transports: transports.concat(additionalTransports),
   });
 
