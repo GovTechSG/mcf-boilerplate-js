4f2a6f15c818314b09b3ca45aa5b1dca618e930e
Wei Jie <leow_wei_jie@tech.gov.sg>
2019-05-06 16:18:42 +0800

    chore: add constants JOB_STATUSES

---
+++ packages/constants/src/mappings/jobStatuses.ts
@@ -0,0 +1,20 @@
+import {JOB_STATUSES as MCF_JOB_STATUSES} from '../mcf';
+import {JOB_STATUSES as ICMS_JOB_STATUSES} from '../icms';
+
+const ICMS_MCF_JOB_STATUSES_MAPPING_TABLE: Array<[number, number]> = [[9, 1], [83, 2], [102, 3], [103, 4]];
+
+const map = <T extends {id: any}>(
+  mappings: Array<[string | number, string | number]>,
+  targetCollection: T[],
+  id,
+  {sourceIndex = 0, targetIndex = 1} = {},
+) => {
+  const mappingFound = mappings.find((mapping) => mapping[sourceIndex] === id) || [];
+  return targetCollection.find((item) => item.id === mappingFound[targetIndex]);
+};
+
+export const mapIcmsToMcfJobStatuses = (id: number) =>
+  map(ICMS_MCF_JOB_STATUSES_MAPPING_TABLE, MCF_JOB_STATUSES, id);
+
+export const mapMcfToIcmsJobStatuses = (id: number) =>
+  map(ICMS_MCF_JOB_STATUSES_MAPPING_TABLE, ICMS_JOB_STATUSES, id, {sourceIndex: 1, targetIndex: 0});
