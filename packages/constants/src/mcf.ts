7fae9c150eac592ca2dae4ee70f0d5c6d037334b
Andrey Bodoev <andrey.bodoev@zenika.com>
2019-05-22 11:34:21 +0800

    feat: add start and expiry dates to mcf schemes

--- packages/constants/src/mcf.ts
+++ packages/constants/src/mcf.ts
@@ -124,10 +124,10 @@ export interface IScheme {
 }
 // no auto conversion from MSF for the moment
 export const SCHEMES = [
-  {id: 1, scheme: 'P-Max'},
-  {id: 2, scheme: 'Professional Conversion Programme (PCP)'},
-  {id: 3, scheme: 'Career Trial'},
-  {id: 4, scheme: 'Career Support'},
+  {id: 1, scheme: 'P-Max', start_date: '1977-05-25', expiry_date: '2055-05-04'},
+  {id: 2, scheme: 'Professional Conversion Programme (PCP)', start_date: '1977-05-25', expiry_date: '2055-05-04'},
+  {id: 3, scheme: 'Career Trial', start_date: '1977-05-25', expiry_date: '2055-05-04'},
+  {id: 4, scheme: 'Career Support', start_date: '1977-05-25', expiry_date: '2055-05-04'},
 ];
 
 /*************************************
